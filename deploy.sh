f4b1b748df76d0088d80259137079d16c2fdbf02
Swati Suresh <65075557+swatisur79@users.noreply.github.com>
2023-01-30 23:06:20 -0500

    small test to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ set -e
 
 REMOTE=$(git remote get-url origin)
 
-DEPLOY_BRANCH="gh-pages"
+DEPLOY_BRANCH="master"
 DEPLOY_DIR="dist"
+DEPLOY_DIR2="apps"
 
 echo "Checking for yarn..."
 if ! command -v yarn; then
@@ -25,14 +26,15 @@ if ! yarn build; then
 	echo "Failed to build deployment, exiting."
 fi
 
-cd $DEPLOY_DIR
+#  cd $DEPLOY_DIR
 
 echo "Initializing git in build directory.."
-git init . -b gh-pages
+git init . -b master
 git remote add origin $REMOTE
 git fetch origin $DEPLOY_BRANCH
 
 echo "Deploying..."
 git add .
 git commit -m "Deployment to ubacm.org"
-git push --force origin gh-pages
+git push --force origin master
+
